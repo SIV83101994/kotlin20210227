22db6672701c55bd90fcc52368aecc9fd15088a5
MB-study <j.s.minging@gmail.com>
2021-02-27 12:28:54 +0800

    kotlin

---
+++ src/main/kotlin/com/ktest/KHello.kt
@@ -0,0 +1,20 @@
+package com.ktest
+
+import com.jtest.JHello
+
+class KHello {
+    fun callMe(){
+
+        print("HAHA")
+    }
+}
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin!")
+    // 呼叫 JHello 的 callMe
+    val j  = JHello()
+    j.callMe()
+    val k = KHello()
+    k.callMe()
+
+}
