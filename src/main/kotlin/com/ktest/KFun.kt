c7a1ae94289005fa77b406423da51d27b5d9d852
MB-study <j.s.minging@gmail.com>
2021-02-27 16:27:04 +0800

    kotlin

---
+++ src/main/kotlin/com/ktest/KFun.kt
@@ -0,0 +1,33 @@
+package com.ktest
+
+class KFun {
+}
+
+//存取修飾字(預設是public)  方法名稱(參數名1 : 參數型態 , 參數名1 : 參數型態...)回傳型態{
+// 方法區塊實作
+// return 回傳結果
+// }
+
+fun  add(x:Int , y:Int):Int{
+    val sum = x+y
+    return sum
+}
+
+fun sub(x: Int,y: Int = 0):Int{
+    val sum = x - y
+    return sum
+}
+fun mul(x: Int = 0 , y: Int = 0):Int{
+    val sum = x * y
+    return sum
+}
+fun div(x: Int = 0 , y: Int = 0):Int{
+    val sum = x / y
+    return sum
+}
+fun main(args: Array<String>){
+    println(add(10, 20))
+    println(sub(30,15))
+    println(mul(14,16))
+    println(div(y=1)) //可以指定y的值
+}
