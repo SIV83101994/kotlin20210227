fac9b8ae5ffd9bfe730de12dfe4f524961baf988
MB-study <j.s.minging@gmail.com>
2021-03-06 12:17:44 +0800

    kotlin

--- src/main/kotlin/com/ktest/Wallet.kt
+++ src/main/kotlin/com/ktest/Wallet.kt
@@ -1,6 +1,6 @@
 package com.ktest
 //錢包
-class Wallet(val name: String, var money: Int ) {
+data class Wallet(val name: String, var money: Int ) {
     //如何檢查傳入的參數是否正確？
     init {
         money = if (money>0) money else 0
