9d3324fb07d3b00bb4933f96925ea23203a19de7
MB-study <j.s.minging@gmail.com>
2021-03-06 14:25:17 +0800

    kotlin

--- src/main/kotlin/com/ktest/KArray.kt
+++ src/main/kotlin/com/ktest/KArray.kt
@@ -19,7 +19,16 @@ fun main() {
     for (i in num3.indices){
         println(num3[i])
     }
-    //四星彩電腦選號(亂數)
+    //四星彩電腦選號(亂數:0-9)
     val num4 = Array(4 ){ _ -> Random.nextInt(10)}  // kotlin: 可以用'_'取代i(當後面不需要用到i參數時)
     println(num4.contentToString()) //Arrays.toString()
+
+    // setter / getter
+    var n1 = num4.get(1) //num4[1] /get->取值
+    println(n1)
+    num4.set(1,9) //num4[1]=9   / set->設定
+    println(num4.contentToString())
+
+    //for-each (印出每個元素內容)
+    num4.forEach { n -> println(n) }
 }
