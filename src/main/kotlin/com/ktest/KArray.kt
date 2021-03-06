f6181e49cd0f5abf31efd5488d35ecc72092caee
MB-study <j.s.minging@gmail.com>
2021-03-06 14:15:56 +0800

    kotlin

--- src/main/kotlin/com/ktest/KArray.kt
+++ src/main/kotlin/com/ktest/KArray.kt
@@ -1,5 +1,7 @@
 package com.ktest
 
+import kotlin.random.Random
+
 fun main() {
     // 數組陣列
     val num1 = arrayOf(1,5,7,3)     //隱式宣告
@@ -12,4 +14,12 @@ fun main() {
     for (i in num1.indices){  //indices-->index範圍
         println(num1[i])
     }
+    //使用Lambda
+    val num3 = Array(5){i -> i * 1 }
+    for (i in num3.indices){
+        println(num3[i])
+    }
+    //四星彩電腦選號(亂數)
+    val num4 = Array(4 ){ _ -> Random.nextInt(10)}  // kotlin: 可以用'_'取代i(當後面不需要用到i參數時)
+    println(num4.contentToString()) //Arrays.toString()
 }
