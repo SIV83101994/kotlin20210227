00ec3d5c771e0b5ac4fd7a21a2e60f57fbc998b2
MB-study <j.s.minging@gmail.com>
2021-03-06 12:38:37 +0800

    kotlin

--- src/main/kotlin/com/ktest/Wallet.kt
+++ src/main/kotlin/com/ktest/Wallet.kt
@@ -4,6 +4,13 @@ data class Wallet(val name: String, var money: Int ) {
     //如何檢查傳入的參數是否正確？
     init {
         money = if (money>0) money else 0
+        //拋出例外
+        money = try {
+                require(money >= 0) //require(boolean判斷式)
+                money
+        }catch (e:Exception){
+            0
+        }
     }
 }
 
