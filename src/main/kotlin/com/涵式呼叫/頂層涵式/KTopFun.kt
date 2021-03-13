35bfafd2ffd563d59579da703b4c55266fa776a7
MB-study <j.s.minging@gmail.com>
2021-03-13 11:37:37 +0800

    kotlin

---
+++ src/main/kotlin/com/涵式呼叫/頂層涵式/KTopFun.kt
@@ -0,0 +1,10 @@
+@file:JvmName("Calc") //Java可以透過此類別名稱調用
+package com.topfun
+
+var count = 0 //頂層變數/頂層屬性
+@JvmName("myAdd")//Java可以透過此類別名稱調用
+fun add(x:Int,y:Int):Int{  //頂層涵式
+    ++count
+    return x + y
+
+}
