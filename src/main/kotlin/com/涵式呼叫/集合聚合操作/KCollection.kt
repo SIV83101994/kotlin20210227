aec7908c9da05ef3e7a434c8e1589de2c3bea334
MB-study <j.s.minging@gmail.com>
2021-03-13 09:55:53 +0800

    kotlin

--- src/main/kotlin/com/涵式呼叫/集合聚合操作/KCollection.kt
+++ src/main/kotlin/com/涵式呼叫/集合聚合操作/KCollection.kt
@@ -1,2 +1,14 @@
 package com.涵式呼叫.集合聚合操作
 
+fun main(){
+    var numbers = listOf(5,42,10,4)
+    println("Count: ${numbers.count()}")
+    println("Sum: ${numbers.sum()}")
+    println("Avg: ${numbers.average()}")
+    println("Max: ${numbers.max()}")
+    println("Min: ${numbers.min()}")
+    println("Max: ${numbers.maxByOrNull { it }}")
+    println("Min: ${numbers.minByOrNull { it }}")
+    //numbers 中 %3 的最小值
+
+}
