b0c1ab3228db1b0fda718e3707c70ed8f493d199
MB-study <j.s.minging@gmail.com>
2021-03-20 12:24:01 +0800

    kotlin

---
+++ src/main/kotlin/com/進階控制/合作例行/Coroutines.kt
@@ -0,0 +1,41 @@
+package com.進階控制.合作例行
+
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun case1(){
+    val startTime = System.currentTimeMillis()
+
+    repeat(10000){
+        Thread{
+            //do something...
+        }.start()
+    }
+
+    val endTime = System.currentTimeMillis()
+    println("case1 : ${endTime - startTime} ms")
+}
+
+fun case2(){
+    //運用coroutines
+    //要先安裝 org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9
+    runBlocking {
+        val startTime = System.currentTimeMillis()
+        val job = launch {
+            repeat(10000){
+                launch {
+                    // do something
+                }
+            }
+        }
+        job.invokeOnCompletion {
+            val endTime = System.currentTimeMillis()
+            println("case2 : ${endTime-startTime} ms")
+        }
+    }
+}
+
+fun main() {
+    case1()
+    case2()
+}
