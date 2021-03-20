5158258b800b182fed33564bf2b197f1853724d3
MB-study <j.s.minging@gmail.com>
2021-03-20 12:05:44 +0800

    kotlin

---
+++ src/main/kotlin/com/進階控制/內聯函數/KInline2.kt
@@ -0,0 +1,17 @@
+package com.進階控制.內聯函數
+
+val sum:(Int,Int) -> Int = {x ,y -> x + y}
+val action: () -> Unit = { println("Kotlin")} // Unit --> Void
+
+//內聯函式多用在高階函式中
+//將函式作用於參數或是返回值的函式
+
+inline fun doCalc(a:Int,b:Int,cal:(a:Int,b:Int)->Int):Int{
+    return cal(a,b)
+}
+
+fun main() {
+    val value = doCalc(10,20, sum)  //sum 可以變成內聯函式
+    print(value)
+    action()
+}
